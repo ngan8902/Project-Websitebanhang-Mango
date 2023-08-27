5743dba91906c7e5613b99ab9575a64f0b5f07ef
MinhAnh83 <127574462+MinhAnh83@users.noreply.github.com>
2023-08-26 20:02:01 -0700

    handle cron for server

--- Server.js
+++ Server.js
@@ -7,6 +7,11 @@ const port = 3000;
 // Middleware
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 app.use(express.static(__dirname + '/public/build'));
 
