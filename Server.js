94dc521a7ac731c87b3b1b1ec4b20586092a4c36
Bich Ngan <bichnganb722@gmail.com>
2023-09-02 10:37:26 +0700

    WIP: 98623e9 implement api GET POST for shop

--- Server.js
+++ Server.js
@@ -1,12 +1,20 @@
 const express = require("express");
+const cors = require('cors');
 const routes = require("./router");
 const mysqlDb = require('./model')
 const app = express();
-const port = 3000;
+const port = 3001;
+const bodyParser = require('body-parser')
 
 // Middleware
+
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+
+app.use(cors())
+
+app.use(bodyParser.json())
+
+app.use(express.urlencoded({extended: false}))
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -18,6 +26,8 @@ app.use(express.static(__dirname + '/public/build'));
 app.use("/api", routes);
 
 mysqlDb.connection();
+
+
 
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
