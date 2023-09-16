1300b7d0ea7c57353ffa279a55c32a67c8330235
ngan8902 <bichnganb722@gmail.com>
2023-09-16 10:49:26 +0700

    WIP: d6c677f WIP: 6ecf860 WIP: 882a6a8 WIP: 0f21406 WIP: 11385f3 add productdata

---
+++ Webapp/public/admin/src/views/Listproduct.js/Product-Quan.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactTable from "react-table";
+
+
+
+
+function Quan () {
+
+
+    return (  
+        <div>  
+           <ReactTable  
+    
+            />  
+        </div>        
+   )
+}
+
+export default Quan;
---
+++ Webapp/public/admin/src/views/Listproduct.js/Product-Vay.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactTable from "react-table";
+
+
+
+
+function Vay () {
+
+
+    return (  
+        <div>  
+           <ReactTable  
+    
+            />  
+        </div>        
+   )
+}
+
+export default Vay;
